077576a14a62e1e651c8bd6ea9c50748ef0abd01
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2022-12-27 22:40:28 +0100

    added a feature

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,6 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://www.naijatechbro.com',
     'http://localhost:7890/',
-    'https://www.google.com',
     'https://www.swagger.io'
 
 ]
