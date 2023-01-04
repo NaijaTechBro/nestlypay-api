507690caa958f0bb5a66ed98e71bd101ee5c4407
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-01-04 12:29:50 +0100

    fixed helper

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,11 +1,11 @@
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://www.naijatechbro.com',
-    'http://localhost:7890/',
-    'https://www.swagger.io',
-    'http://http://127.0.0.1:5173'
+// const allowedOrigins = [
+//     'http://localhost:3000',
+//     'http://www.naijatechbro.com',
+//     'http://localhost:7890/',
+//     'https://www.swagger.io',
+//     'http://http://127.0.0.1:5173'
 
-]
+// ]
 
 
-module.exports = allowedOrigins
+// module.exports = allowedOrigins
