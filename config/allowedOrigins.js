79aaf2be6f719dcf3946cf5346263bbf616651de
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-01-06 11:20:51 +0100

    fix swagger ui

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://www.naijatechbro.com',
     'http://localhost:7890/',
     'https://www.swagger.io',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://nestlipay-data.onrender.com'
 
 ]
 
