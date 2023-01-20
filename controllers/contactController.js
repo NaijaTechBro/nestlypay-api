f44fa206c058b2588722dc6ce33515f981a829ee
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-01-20 14:53:37 +0100

    fixed mail

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,9 +2,8 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const sendEmail = require("../utils/sendEmail");
 
-
 const contactUs = asyncHandler(async (req, res) => {
-    const { subjects, message } = req.body;
+    const { subject, message } = req.body;
     const user = await User.findById(req.user._id);
 
     if (!user) {
@@ -13,36 +12,21 @@ const contactUs = asyncHandler(async (req, res) => {
     }
 
   //   Validation
-    if (!subjects || !message) {
+    if (!subject || !message) {
     res.status(400);
     throw new Error("Please add subject and message");
     }
-
-   //send welcome mail
-  const subject = "Cryptea - Olubori Paul";
-  const send_to = email;
-  const sent_from = process.env.EMAIL_USER;
-  const reply_to = "hello@cryptea.me";
-  const fullname = user.name;
-  const template = "xeus";
-
-  try {
-    await sendEmail(
-      subject,
-      send_to,
-      sent_from,
-      reply_to,
-      template,
-      fullname,
-    );
-    res
-    .status(200)
-    .json({ success: true, message: "Welcome Email Sent"});
-  } catch (error) {
+    const sent_from = "Nestlypay <hello@nestlypay.com>";
+    const send_to = process.env.EMAIL_USER;
+    // const sent_from = process.env.EMAIL_USER;
+    const reply_to = user.email;
+    try {
+    await sendEmail(subject, message, send_to, sent_from, reply_to);
+    res.status(200).json({ success: true, message: "Email Sent" });
+    } catch (error) {
     res.status(500);
     throw new Error("Email not sent, please try again");
-  }
-
+    }
 });
 
 module.exports = {
