9b953c265bc61789fa0ba3d39024a86553d3ec34
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-02-02 22:46:54 +0100

    decrypted code

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,4 +1,4 @@
-const whitelist = ['http://127.0.0.1:7890', 'https://nestlipay-data.com', 'https://nestlypay.vercel.app','http://localhost:7890', 'https://nestlipay-data.onrender.com']
+const whitelist = ['http://127.0.0.1:7890', 'http://127.0.0.1:3000', 'http://127.0.0.1:3001', 'https://nestlipay-data.com', 'https://nestlypay.vercel.app','http://localhost:7890', 'https://nestlipay-data.onrender.com']
 
 module.exports = {
     whitelist
