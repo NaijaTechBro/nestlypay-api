4589818a94678a53bd1fccecc9219f326ce18e35
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-02-05 20:11:08 +0100

    cors

--- server.js
+++ server.js
@@ -50,7 +50,6 @@ const errorHandlerr = require("./middleware/errorMiddleware");
 // Invoice Helper Import
 
 const pdfTemplate = require("./documents/index");
-// const invoiceTemplate = require("./documents/invoice");
 const emailTemplate = require("./documents/email");
 
 // Connecting to Database Environments
@@ -64,7 +63,9 @@ connectDB()
 app.use(logger)
 
 // Cross Origin Resource Sharing
-app.use(cors(['http://nestlypay.vercel.app', 'http://localhost:3001']))
+app.use(cors({
+    origin:"https://nestlypay.vercel.app"
+}))
 
 app.use(express.json({ limit: "30mb", extended: true}))
 app.use(helmet());
@@ -86,25 +87,11 @@ app.use("/api/v1", contactRoutes);
 app.use("/api/v1", businessRoutes);
 
 
-// // NODEMAILER TRANSPORT FOR SENDING INVOICE VIA EMAIL
-// const transporter = nodemailer.createTransport({
-//     host: process.env.EMAIL_HOST,
-//     port : process.env.EMAIL_PORT,
-//     auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS
-//     },
-//     tls:{
-//         rejectUnauthorized:false
-//     }
-// })
-
 var options = { format: 'A4' };
 //SEND PDF INVOICE VIA EMAIL
 app.post('/send-pdf', (req, res) => {
     const { email, company } = req.body
 
-    // pdf.create(pdfTemplate(req.body), {}).toFile('invoice.pdf', (err) => {
     pdf.create(pdfTemplate(req.body), options).toFile('invoice.pdf', async (err) => {
     
           // send mail with defined transport object
@@ -138,29 +125,7 @@ app.post('/send-pdf', (req, res) => {
 });
 });
 
-        
-            // subject : "Welcome to Nestlypay",
-            // send_to : email,
-            // sent_from : process.env.EMAIL_USER,
-            // reply_to : "noreply@nestlypay.com",
-            // template : "welcome",
-            
-            // sent_from: process.env.EMAIL_USER, // sender address
-            // send_to: `${email}`,  // to: `${email}`, // list of receivers
-            // replyTo: `${company.email}`,
-            // subject: `Invoice from ${company.businessName ? company.businessName : company.name}`, // Subject line
-            // text: `Invoice from ${company.businessName ? company.businessName : company.name }`, // plain text body
-            // html: emailTemplate(req.body), // html body
-
-//             attachments: [{
-//                 filename: 'invoice.pdf',
-//                 path: `${__dirname}/invoice.pdf`
-//             }]
-
-//         if(err) {
-//             res.send(Promise.reject());
-//         }
-//         res.send(Promise.resolve());
+    
 
 //Problems downloading and sending invoice
 // npm install html-pdf -g
