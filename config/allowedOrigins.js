340346bd1c1bbfbbdd7f2ca781193bf3cc2cad54
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-02-06 21:38:14 +0100

    hellop

---
+++ config/allowedOrigins.js
@@ -0,0 +1,13 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'http://www.naijatechbro.com',
+    'http://localhost:7890/',
+    'https://www.swagger.io',
+    'https://www.google.com',
+    'http://127.0.0.1:5173',
+    'https://nestlipay-data.onrender.com'
+
+]
+
+
+module.exports = allowedOrigins
