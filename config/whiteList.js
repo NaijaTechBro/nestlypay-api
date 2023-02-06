8d5153ef7c0a3488032608d2cb34a0a7204bc622
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-02-06 21:41:29 +0100

    hellop

---
+++ config/whiteList.js
@@ -0,0 +1,5 @@
+const whitelist = ['http://127.0.0.1:7890', 'http://nestlypay.vercel.app', 'http://localhost:3000', 'http://localhost:3001', 'https://nestlipay-data.com', 'https://nestlypay.vercel.app','http://localhost:7890', 'https://nestlipay-data.onrender.com']
+
+module.exports = {
+    whitelist
+}
